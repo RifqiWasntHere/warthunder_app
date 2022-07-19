b19a7d2a388d72d9e8d744187dc41701b565e3db
RifqiNotWibu <rifqifadhillahh@gmail.com>
2022-07-19 14:38:49 +0700

    Added tankControllers

---
+++ controllers/tankControllers.js
@@ -0,0 +1,21 @@
+const { Tanks } = require("../models");
+
+class tankControllers {
+  static async addTank(req, res, next) {
+    try {
+      const { tankName, turretId, armorScore, engine, countryId } = req.body;
+      let addTank = await Tanks.create({
+        tankName,
+        turretId,
+        armorScore,
+        engine,
+        countryId,
+      });
+      res.status(201).json({ status: "Tank successfully added." });
+    } catch (err) {
+      next(err);
+    }
+  }
+}
+
+module.exports = tankControllers;
