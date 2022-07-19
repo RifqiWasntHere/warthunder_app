e9b25b9f584fc514feb365dfe522156e6bb6bd5b
RifqiNotWibu <rifqifadhillahh@gmail.com>
2022-07-19 17:08:36 +0700

    Added countryRoutes into index.js

--- routers/index.js
+++ routers/index.js
@@ -2,7 +2,14 @@ const express = require("express");
 const router = express.Router();
 
 const userControllers = require("../controllers/userControllers");
+const countryRoutes = require("./countries");
+const featureRoutes = require("./features");
+const penetrationRoutes = require("./penetrations");
+const shellRoutes = require("./shells");
+const tankRoutes = require("./tanks");
+const turretRoutes = require("./turrets");
 
 router.post("/register", userControllers.register);
+router.use("/addCountry", countryRoutes);
 
 module.exports = router;
