7ee43cd685dc39831c46a3702875cb136183c4b7
RifqiNotWibu <rifqifadhillahh@gmail.com>
2022-07-21 18:24:04 +0700

    Added shellRoutes endpoint

--- routers/index.js
+++ routers/index.js
@@ -3,6 +3,7 @@ const countryControllers = require("../controllers/countryControllers");
 const router = express.Router();
 
 const userControllers = require("../controllers/userControllers");
+
 const countryRoutes = require("./countries");
 const featureRoutes = require("./features");
 const penetrationRoutes = require("./penetrations");
@@ -17,5 +18,7 @@ router.get("/showUsers", userControllers.userList); //Test sequelize
 router.use("/country", countryRoutes);
 
 router.use("/turret", turretRoutes);
+
+router.use("/shell", shellRoutes);
 
 module.exports = router;
