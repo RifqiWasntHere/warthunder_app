1c20a7b0f98e588f955b592926e0fb57f7ac12cc
RifqiNotWibu <rifqifadhillahh@gmail.com>
2022-07-21 18:24:20 +0700

    Added shellList endpoint

--- routers/shells.js
+++ routers/shells.js
@@ -3,6 +3,8 @@ const router = express.Router();
 
 const shellControllers = require("../controllers/shellControllers");
 
+router.get("/", shellControllers.shellList);
+
 router.post("/addShell", shellControllers.addShell);
 
 module.exports = router;
