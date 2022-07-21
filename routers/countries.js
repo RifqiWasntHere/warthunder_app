189afebf41b24b40401652c99a07ac07c03d07e4
RifqiNotWibu <rifqifadhillahh@gmail.com>
2022-07-21 18:37:54 +0700

    Tidying some rows

--- routers/countries.js
+++ routers/countries.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const countryControllers = require("../controllers/countryControllers");
 
 router.get("/", countryControllers.countryList);
+
 router.post("/addCountry", countryControllers.addCountry);
 
 module.exports = router;
