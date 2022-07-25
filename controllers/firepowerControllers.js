c3dee603c675fae85644d9512114c084dea63ac2
RifqiNotWibu <rifqifadhillahh@gmail.com>
2022-07-25 11:54:50 +0700

    Aded firepowerControllers with addFirepower and firepowerList method

---
+++ controllers/firepowerControllers.js
@@ -0,0 +1,28 @@
+const { Firepowers } = require("../models");
+
+class firepowerControllers {
+  static async addFirepower(req, res, next) {
+    try {
+      const { turretId, shellId, penetrationId } = req.body;
+      let addFirepower = await Firepowers.create({
+        turretId,
+        shellId,
+        penetrationId,
+      });
+      res.status(201).json({ status: "firepower successfully added." });
+    } catch (err) {
+      next(err);
+    }
+  }
+
+  static async firepowerList(req, res, next) {
+    try {
+      let firepowerList = await Firepowers.findAll({ raw: true });
+      res.status(200).json(firepowerList);
+    } catch (err) {
+      next(err);
+    }
+  }
+}
+
+module.exports = firepowerControllers;
