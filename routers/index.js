04001860f39f4525e1c7953d93aa3cbfc6c87d9d
RifqiNotWibu <rifqifadhillahh@gmail.com>
2022-07-25 14:43:15 +0700

    Added tankFeatureRoutes endpoint

--- routers/index.js
+++ routers/index.js
@@ -10,6 +10,7 @@ const shellRoutes = require("./shells");
 const tankRoutes = require("./tanks");
 const turretRoutes = require("./turrets");
 const firepowerRoutes = require("./firepowers");
+const tankFeatureRoutes = require("./tankfeatures");
 
 router.post("/register", userControllers.register);
 
@@ -26,5 +27,7 @@ router.use("/tank", tankRoutes);
 router.use("/penetration", penetrationRoutes);
 
 router.use("/firepower", firepowerRoutes);
+
+router.use("/tankFeature", tankFeatureRoutes);
 
 module.exports = router;
