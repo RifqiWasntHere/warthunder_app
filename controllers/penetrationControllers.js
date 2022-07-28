8fe67a94a5d8f115eac0b96d8d65fc94c57a746e
RifqiNotWibu <rifqifadhillahh@gmail.com>
2022-07-28 09:13:33 +0700

    Added updatePenetration method along & reformatted

--- controllers/penetrationControllers.js
+++ controllers/penetrationControllers.js
@@ -3,9 +3,24 @@ const { Penetrations } = require("../models");
 class penetrationControllers {
   static async addPenetration(req, res, next) {
     try {
-      const { description } = req.body;
-      let addPenetration = await Penetrations.create({ description });
+      let addPenetration = await Penetrations.create({
+        description: req.body.description,
+      });
       res.status(201).json({ status: "Penetration data successfully added." });
+    } catch (err) {
+      next(err);
+    }
+  }
+  static async updatePenetration(req, res, next) {
+    try {
+      const { description, targetId } = req.body;
+      let updatePenetration = await Penetrations.update(
+        { description },
+        { where: { id: targetId } }
+      );
+      res
+        .status(201)
+        .json({ status: "Penetration data successfully updated." });
     } catch (err) {
       next(err);
     }
